c1a59284b378eb7c26650088bbcad1276fcc4ca8
NifantovaIrina <nifantova98@gmail.com>
2020-10-25 11:00:18 +0300

    wip

--- React-Landing-Page-Template/src/components/about.jsx
+++ React-Landing-Page-Template/src/components/about.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export class about extends Component {
   render() {
     return (
-                <div className="wrapper">
+        <div className="wrapper">
 
         <div id="about">
         <div className="container">
---
+++ React-Landing-Page-Template/src/components/mayBeInteresting.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+export class recommend extends Component {
+  render() {
+    return (
+        <div className="wrapper">
+        <div id="shares">
+        <div className="container">
+          <div className="row">
+            {/*<div className="col-xs-12 col-md-6"> <img src="img/about.jpg" className="img-responsive" alt=""/> </div>*/}
+            <div className="col-xs-12 col-md-12">
+              <div className="about-text">
+                <h2>Вас может заинтересовать</h2>
+{/*                 <p>{this.props.data ? this.props.data.paragraph : 'loading...'}</p> */}
+
+{/*                     <div className="col-lg-6 col-sm-6 col-xs-12"> */}
+{/*                     <ul> */}
+{/*                     {this.props.data ? this.props.data.Why.map((d, i) => <li  key={`${d}-${i}`}> {d}</li>) : 'loading'} */}
+{/*                     </ul> */}
+{/*                   </div> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+            </div>
+    )
+  }
+}
+
+export default shares
